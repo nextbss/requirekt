d20c1e48dbf4fabbaba3aa19de66392b3f3eafb5
alexjuca <alexandre.juca@nextbss.co.ao>
2020-09-04 09:08:54 +0100

    modified custom error response

--- src/main/kotlin/co/ao/nextbss/requirekt/annotation/CustomErrorResponse.kt
+++ src/main/kotlin/co/ao/nextbss/requirekt/annotation/CustomErrorResponse.kt
@@ -1,4 +1,11 @@
 package co.ao.nextbss.requirekt.annotation
 
-interface CustomErrorResponse {
-}
+import org.springframework.stereotype.Component
+
+@Target(AnnotationTarget.TYPE, AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@MustBeDocumented
+@Component
+annotation class CustomErrorResponse(
+
+)
